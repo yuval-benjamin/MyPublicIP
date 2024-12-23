082669d402dca358e3b5c2baeeca47cf12de396d
yuval-benjamin <jjuunnkkmm@gmail.com>
2024-12-23 17:17:41 +0200

    feat: adding the app and a Dockerfile

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+# from flask import request, jsonify
+import requests
+
+# Returns the public IP Address of the user
+def get_public_ip():
+    response = requests.get('https://api.ipify.org?format=json')
+    public_ip = response.json()['ip']
+    return public_ip
+
+
+# Create a Flask app
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    public_ip = get_public_ip()
+    return render_template('index.html',public_ip = public_ip)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
